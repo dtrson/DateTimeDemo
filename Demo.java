705f768067a50e201fb5ced7b5d7124a5c55c397
Son-Truong Duong <sduong@sama.int>
2017-07-20 13:34:15 +0200

    added DateTimeDemo program

---
+++ Demo.java
@@ -0,0 +1,12 @@
+
+import java.util.Date;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println("Date Time: " + date.toString());
+
+	}
+
+}
